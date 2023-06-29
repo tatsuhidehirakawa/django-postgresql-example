a94bc64e123c05c686ef981968e0c7db3dc5144a
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2023-06-29 18:40:56 +0900

    Fixed codes.

--- s_01_src/124dja_src/config/urls.py
+++ s_01_src/124dja_src/config/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include # <- added.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')) # <- added.
 ]
