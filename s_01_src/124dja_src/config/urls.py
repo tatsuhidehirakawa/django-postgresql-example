af504328c14a461894277df09af4938b2244f336
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@stgprd.com>
2023-07-03 21:15:52 +0900

    Added router module.

--- s_01_src/124dja_src/config/urls.py
+++ s_01_src/124dja_src/config/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include # <- added.
+from rest_framework import routers, serializers, viewsets
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')) # <- added.
 ]
