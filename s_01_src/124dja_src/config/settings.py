ea4ac253be40c3c8963a3664255b60b79de1bc09
FirstThinkD <thinkd.developer@gmail.com>
2023-07-09 17:34:02 +0900

    plansエンドポイント

--- s_01_src/124dja_src/config/settings.py
+++ s_01_src/124dja_src/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',               # <- added.
+    'plans',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
