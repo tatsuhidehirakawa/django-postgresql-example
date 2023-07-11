463c4c459690d7a83ed3c32857f200fdd3218349
FirstThinkD <thinkd.developer@gmail.com>
2023-07-11 17:02:11 +0900

    Update urls.py

--- s_01_src/124dja_src/plans/urls.py
+++ s_01_src/124dja_src/plans/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.get, name='plans'),
     path('somePost', views.create, name='plans-create'),
-    path('someGet/<int:pk>', views.read, name='plans-read'),
-    path('somePut/<int:pk>', views.update, name='plans-update'),
-    path('someDelete/<int:pk>', views.delete, name='plans-delete'),
+    path('someGet/<str:pk>', views.read, name='plans-read'),
+    path('somePut/<str:pk>', views.update, name='plans-update'),
+    path('someDelete/<str:pk>', views.delete, name='plans-delete'),
 ]
