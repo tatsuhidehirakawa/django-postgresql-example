cfe7402d4eee1cc1f036bc8ac2c997a3083b346e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@stgprd.com>
2023-07-15 16:54:32 +0900

    Add instanse for test.

--- s_01_src/124dja_src/plans/serializers.py
+++ s_01_src/124dja_src/plans/serializers.py
@@ -5,4 +5,4 @@ from .models import OfferListModel
 class OfferListSerializer(serializers.ModelSerializer):
     class Meta:
         model = OfferListModel
-        fields = ['account_id', 'offer_type', 'offer_title', 'offer_severity', 'offer_catagory', 'offer_location', 'offer_status', 'offer_text']
+        fields = ['offer_id','account_id', 'offer_type', 'offer_title', 'offer_severity', 'offer_catagory', 'offer_location', 'offer_status', 'offer_text']
